4a6c8b298298b6867ec6f4eecfb4c2c35357ee91
TMcCool <tjmccool1@icloud.com>
2018-04-20 21:18:47 -0500

    initial commit

---
+++ src/main/java/com/thomasmccool/bartender/BartenderApplication.java
@@ -0,0 +1,12 @@
+package com.thomasmccool.bartender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BartenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BartenderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/thomasmccool/bartender/BartenderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thomasmccool.bartender;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BartenderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
